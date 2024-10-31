c94f7ada94a015bdebe68c595511b857f4736d8f
argonoid48 <158807187+argonoid48@users.noreply.github.com>
2024-10-31 18:48:42 +0300

    Add files via upload

---
+++ module_3_2.py
@@ -0,0 +1,18 @@
+# Галкин Андрей
+# Задача "Рассылка писем"
+
+def send_email(message, recipient, * ,sender = 'university.help@gmail.com'):
+    if '@' not in recipient or '@' not in sender or not recipient.endswith(('.ru', '.com', '.net')) or not sender.endswith(('.ru', '.com', '.net')) :
+        print('Невозможно отправить письмо с адреса ', sender, 'на адрес ', recipient)
+    elif recipient == sender:
+        print('Нельзя отправить письмо самому себе!')
+    elif sender != 'university.help@gmail.com':
+        print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса ', sender, 'на адрес ',recipient)
+    else:
+        print('Письмо успешно отправлено с адреса ', sender, 'на адрес ',recipient)
+#   print(message, recipient,sender)
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
